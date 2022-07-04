dcbe8c886ca2feb078dbeab4d252733bdefc5209
hector <sheep123_yang@163.com>
2022-07-04 09:47:03 +0800

    feature: add gsap website

---
+++ gsap/car.js
@@ -0,0 +1,66 @@
+import { gsap } from "gsap";
+import { car as car_info } from "./src/data/db.json"
+
+class Car {
+	data = []
+
+	constructor(info) {
+		info? this._create(info):null
+	}
+	_create = (info) => {
+		info.forEach(car => {
+			console.log("Car:", car)
+			const dom = document.querySelector(car.id)
+			const effect = gsap.to(dom, {
+				duration: car.duration,
+				ease: car.ease,
+				x: car.position.x,
+				y: car.position.y,
+				repeat: car.repeat,
+				repeatDelay: car.repeatDelay,
+				paused: true
+			})
+
+			const audio = new Audio(`./src/sound/${car.sound_effect}`)
+			const listener = this._addListener(dom, effect, audio)
+
+			this.data.push({dom, effect, listener})
+		})
+	}
+
+	_addListener = (dom, effect, audio) => {
+		let timer = null
+		const click = dom.addEventListener("click", () => {
+			// effect.paused(!effect.paused)
+			this._clearTimer(timer)
+			timer = setTimeout(() => {
+				console.log(effect.paused)
+				effect.paused(!effect.paused)
+			}, 200)
+
+		})
+		const dblClick = dom.addEventListener("dblclick", () => {
+			this._clearTimer(timer)
+			audio.play()
+		})
+		return {click, dblClick}
+	}
+	_clearTimer = (timer) => {
+		timer && clearTimeout(timer)
+	}
+
+	play = () => {
+		this.data.forEach(car => {
+			car.effect.play()
+		})
+	}
+
+	paused = () => {
+		this.data.forEach(car => {
+			car.effect.paused()
+		})
+	}
+}
+
+const car = new Car(car_info)
+export {car}
